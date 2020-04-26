94710bddada57522e3049d0f73a9a2eee3af1e8d
fengl <fenglex@126.com>
2020-04-26 22:30:28 +0800

    配置eureka

--- simple-web-service/src/main/java/com/fenglex/cloud/web/CloudApplication.java
+++ simple-web-service/src/main/java/com/fenglex/cloud/web/CloudApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient(autoRegister = false)
+//@EnableDiscoveryClient(autoRegister = false)
 public class CloudApplication {
 
 	public static void main(String[] args) {
