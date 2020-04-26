4b8389dfbf0c784123d2ab18b7b2c574eb811d2f
fengl <fenglex@126.com>
2020-04-26 22:30:12 +0800

    配置eureka

--- eureka-server/src/main/java/com/fenglex/eureka/EurekaApplication.java
+++ eureka-server/src/main/java/com/fenglex/eureka/EurekaApplication.java
@@ -1,9 +1,21 @@
 package com.fenglex.eureka;
 
+import com.netflix.appinfo.DataCenterInfo;
+import com.netflix.appinfo.MyDataCenterInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.StandardEnvironment;
+
+import java.util.Date;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class);
--- simple-web-service/src/main/java/com/fenglex/cloud/web/CloudApplication.java
+++ simple-web-service/src/main/java/com/fenglex/cloud/web/CloudApplication.java
@@ -2,8 +2,11 @@ package com.fenglex.cloud.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient(autoRegister = false)
 public class CloudApplication {
 
 	public static void main(String[] args) {
