8fc3e2e71da28644ececc438d0486b027b667b28
fengl <fenglex@126.com>
2020-04-28 20:52:16 +0800

    配置eureka认证信息

---
+++ eureka-server-auth/src/main/java/com/fenglex/WebSecurityConfigure.java
@@ -0,0 +1,16 @@
+package com.fenglex;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //http.csrf().disable();
+        super.configure(http);
+    }
+}
