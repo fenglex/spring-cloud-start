199fb656d8590ab23aa9cebdba10d0b67d99506f
fengl <fenglex@126.com>
2020-05-08 21:32:34 +0800

    配置eureka认证信息

--- simple-web-service/src/main/java/com/fenglex/cloud/web/CloudApplication.java
+++ simple-web-service/src/main/java/com/fenglex/cloud/web/CloudApplication.java
@@ -4,13 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 //@EnableDiscoveryClient(autoRegister = false)
+@EnableScheduling
 public class CloudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CloudApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CloudApplication.class, args);
+    }
 
+
+    @Scheduled(cron = "0 42 12 * * *")
+    public void test() {
+
+        System.out.println(1111111111);
+    }
 }
